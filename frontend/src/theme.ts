5592055bd11d8bdcfa34a43e217ca78f5ed0eb01
Drashna Jaelre <drashna@live.com>
2021-07-27 17:18:39 -0700

    Update theme.ts

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -3,10 +3,10 @@ import { createMuiTheme } from "@material-ui/core/styles";
 export default createMuiTheme({
   palette: {
     primary: {
-      main: "##9580ff",
+      main: "#9580ff",
     },
     secondary: {
-      main: "##9580ff",
+      main: "#9580ff",
     },
   },
 });
